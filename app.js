27023c263aed5c29aa403cd12d259bdd03bd8d0c
谷太海 <TAIGA07@MacBook-Air.local>
2021-12-04 15:11:56 +0900

    [finish]5章

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 $(function(){
-  $('.box1').slideUp();
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color':'#0000ff',
+      'height':'100px',
+      'width':'200px'
+    }).slideUp();
+  });
 });
