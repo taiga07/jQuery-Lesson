d0cc844946b4d59717951d1488ba227f1556d583
taiga07 <taigatani@me.com>
2022-02-28 00:00:51 +0900

    [practice]JQuery

--- app.js
+++ app.js
@@ -71,11 +71,37 @@
 
 
 
+// $(function(){
+//   $('.box1').on('click',function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+
+// $(function(){
+//   $('.bg1').on('click',function(){
+//     $('.bg1').slideUp();
+//   });
+
+//   $('.bg2').on('click',function(){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click',function(){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click',function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+
 $(function(){
   $('.box1').on('click',function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+    $(this).slideUp();
   });
 });
