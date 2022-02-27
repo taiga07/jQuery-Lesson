cb7bad9534053181c75235085075725185ffa38d
taiga07 <taigatani@me.com>
2022-02-27 19:28:43 +0900

    [practice]JQuery

--- app.js
+++ app.js
@@ -32,12 +32,50 @@
 // });
 
 
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//       'background-color':'#0000ff',
+//       'height':'100px',
+//       'width':'200px'
+//     }).slideUp();
+//   });
+// });
+
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color':'#0000ff'});
+//   });
+// });
+
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color':'#0000ff'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color':'#ff0000'});
+//   });
+// });
+
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+
+
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color':'#0000ff',
-      'height':'100px',
-      'width':'200px'
-    }).slideUp();
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
